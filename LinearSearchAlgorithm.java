84525f1244defcfee0c1716c07f71a7aaf6128d4
Nayana Harish <harish0878@saskpolytech.ca>
2024-08-27 13:15:23 -0600

    Linear search algorithm - search a target value

---
+++ LinearSearchAlgorithm.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class LinearSearchAlgorithm {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int arr [] = {1,4,7,2,3,9,1,-1};
+        System.out.println(findLinearSearch(arr, -1));
+
+
+    }
+
+
+
+    static int findLinearSearch(int arr[],int target)
+    {
+
+        if (arr.length > 0)
+        {
+            for (int index=0; index <= arr.length-1 ; index++)
+            {
+                if (target== arr[index])
+                {
+                    return index;
+                }
+            }
+        }
+        return -1;
+    }
+}
